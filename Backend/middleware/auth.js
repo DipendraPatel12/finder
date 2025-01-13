69c694d0452f199ce9b71e787c0187d2b2738549
DipendraPatel12 <dipendrapatel926@gmail.com>
2025-01-13 19:22:32 +0530

    minor changes

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -4,14 +4,23 @@ import dotenv from "dotenv";
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET; // Use a strong secret key or an environment variable
 
-export const authenticateToken = (req, res, next) => {
-  const token = req.cookies.authToken;
 
-  if (!token) return res.status(401).json({ message: "No token provided" });
+
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Token not found" });
+  }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: "Invalid token" });
-    req.user = user; // Attach user info to the request object
+    if (err) {
+      return res.status(403).json({ message: "Token is not valid" });
+    }
+    req.user = user;
     next();
   });
 };
+
+
--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,9 @@ import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js';
 import dotenv from "dotenv";
 const corsOptions = {
-  origin: ["https://finder-pi-lilac.vercel.app"],
+  origin: ["https://finder-pi-lilac.vercel.app",
+    "http://localhost:5174"
+  ],
   credentials: true // Allow cookies to be sent with requests
 };
 app.use(cors(corsOptions));
